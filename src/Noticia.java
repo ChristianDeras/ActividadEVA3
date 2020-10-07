d7a239a46ecb64a228b62a114d925d972357fb33
Alberto400 <71241801+Alberto400@users.noreply.github.com>
2020-10-06 23:28:14 -0600

    Creando carpeta src

---
+++ src/Noticia.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+package clases;
+public class Noticia
+{
+	public String Titulo,FechaPublicada,Categoria;
+	Scanner leer = new Scanner(System.in);
+	
+	public void Insertar()
+	{
+		
+	}
+}
